79d0c77b396db73bcec9a41229b2eeb2156193fe
Joshua Baert <jbghoring@gmail.com>
2017-04-24 16:37:55 -0500

    Practice makes perfect

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  template:
+    `
+<h3>Servers Component</h3>
+<app-server></app-server>
+<app-server></app-server>
+<app-server></app-server>
+`,
 })
 export class ServersComponent implements OnInit {
 
